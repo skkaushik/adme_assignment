92cacb7858b5a07511a9becdd1b3d1f0d1ac1a71
sachin <sksachinkaushik5362@gmail.com>
2023-09-06 10:29:27 +0530

    navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <div class="navbar">
+        <div class="logo">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLAYm3SKpyuS9NC4qSgo3ZJlowldKHmQfSrQ&usqp=CAU" alt="Logo"/>
+        </div>
+        <div class="brand">
+            React Gallery
+        </div>
+    </div>
+    </>
   )
 }
 
